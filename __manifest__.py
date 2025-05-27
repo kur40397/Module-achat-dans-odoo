6fe4e572d9ea4acc283add11da2226f114ad8265
badr <badr.ahrir1@gmail.com>
2025-05-27 13:47:17 +0000

    Sprint 1 - Création du menu Bon de commande

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Module Achat de Olympe industries',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/bon_commande_views.xml',
+        #'views/bon_reception_views.xml',
+        #'views/ligne_stock_views.xml',
+        #'views/produit_prix_fournisseurs_views.xml',
+        'views/menu_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ models/bon_commande.py
@@ -0,0 +1,4 @@
+from odoo import models , fields,api
+
+class BonCommande(models.Model):
+    _name = "module.achat.bon.commande"
