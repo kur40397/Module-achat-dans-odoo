7164c2cd48f25bd388ab36afac77ce4149df6d71
badr <badr.ahrir1@gmail.com>
2025-07-03 11:41:56 +0000

    print 35 – Création d’un modèle produit_personnalise

--- models/produit.py
+++ models/produit.py
@@ -1,27 +1,33 @@
 from odoo import fields,api , models
+from odoo.api import readonly
 from odoo.exceptions import UserError
 
 
 class Produit(models.Model):
-    _inherit = "product.template"
+    _name = "module_achat.produit"
+    nom=fields.Char(string="Nom")
+    reference=fields.Char(string="Reference")
+    categorie=fields.Selection(
+        [
+            ('equipements_protection_incendie','Équipements de protection incendie'),
+            ('Accessoires_consommables','Accessoires & consommables'),
+            ('Pieces_rechange','Pièces de rechange'),
+            ('Materiaux_test_controle','Matériaux de test & contrôle')
+        ]
+    ,string="Categorie",required=True)
+
     norme=fields.Char(string="Norme",required=True)
-    default_code=fields.Char(required=True)
+    unite_mesure=fields.Many2one("uom.uom",string="unite mesure",required=True)
     state = fields.Selection([
         ('draft', 'Brouillon'),
         ('valide', 'Valide'),
     ], default="draft")
-    prix_fournisseur_ids=fields.One2many("module_achat.prix_fournisseur","produit_id")
+    prix_fournisseur_ids=fields.One2many("module_achat.prix_fournisseur","prod_id")
     def valider_produit(self):
         # met a jour et enregistre tout de suite a la base
         # par contre self.state = 'valide' sauvegarde dans la mémoire
-        erreur=[]
-        if self.weight < 0:
-            erreur.append("le poid doit être positive")
-        if self.volume <0:
-            erreur.append("le volume doit être positive")
 
-        if len(erreur) != 0:
-            raise UserError("\n".join(erreur))
+
 
         self.write({
             'state':'valide'
@@ -30,7 +36,7 @@ class Produit(models.Model):
 class PrixFournisseur(models.Model):
     _name = "module_achat.prix_fournisseur"
     reference_fournisseur=fields.Char("reference fournisseur",required=True)
-    produit_id=fields.Many2one("product.template",string="Produit",required=True)
+    prod_id=fields.Many2one("module_achat.produit",string="Produit",required=True)
     ref_fournisseur=fields.Many2one("res.partner",string="Fournisseur",required=True)
     prix_unitaire=fields.Float(string="Prix",required=True)
     devise_id =fields.Many2one("res.currency",string="Devise",required=True)
